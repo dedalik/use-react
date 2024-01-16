0c3abf0f31317957ac536db6c3707d8f0fa524f5
Radiks Alijevs <dedalik@gmail.com>
2024-01-17 00:04:12 +0200

    Add new hooks (#1)

--- __tests__/useTextareaAutoSize.test.tsx
+++ __tests__/useTextareaAutoSize.test.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import { render, fireEvent, screen, renderHook, act } from '@testing-library/react'
-import { useTextareaAutoSize } from '../src/hooks/Elements/useTextareaAutoSize/index'
-import TextareaAutoSize from '../src/components/Elements/TextareaAutoSize/TextareaAutoSize'
+import TextareaAutoSize from '../src/components/Browser/TextareaAutoSize/TextareaAutoSize'
+import { useTextareaAutoSize } from '../src'
 
-describe('useTextareaAutosize', () => {
+describe('useTextareaAutoSize', () => {
   it('initializes textarea and input ref with default values', () => {
+    // Render the hook and capture the result
     const { result } = renderHook(() => useTextareaAutoSize())
 
-    expect(result.current.textarea.current).toBeNull()
+    // Check if the textarea ref is initialized and points to null.
+    // This ensures that the hook correctly initializes the ref.
+    expect(result.current.textareaRef.current).toBeNull()
+
+    // Check if the input state is initialized and is undefined.
+    // This is the expected initial state for the input.
     expect(result.current.input).toBeUndefined()
+
+    // Check if the setInput function is defined.
+    // This function is part of the hook's return value and should be available.
+    expect(typeof result.current.setInput).toBe('function')
+
+    // Check if the triggerResize function is defined.
+    // This function is crucial for adjusting the textarea's size and should be available.
+    expect(typeof result.current.triggerResize).toBe('function')
   })
 
   it('updates the input value when setInput is called', () => {
@@ -28,10 +42,54 @@ describe('useTextareaAutosize', () => {
     // Mocking the scrollHeight property
     Object.defineProperty(textarea, 'scrollHeight', {
       writable: true,
-      value: 100, // The pixel height you expect after changing the content
+      value: 150, // The pixel height you expect after changing the content
     })
 
     fireEvent.change(textarea, { target: { value: 'New text' } })
-    expect(textarea.style.height).toBe('100px')
+    expect(textarea.style.height).toBe('150px')
+  })
+
+  it('initializes textarea with external elementRef', () => {
+    const externalRef = { current: document.createElement('textarea') }
+    const { result } = renderHook(() => useTextareaAutoSize({ elementRef: externalRef }))
+
+    expect(result.current.textareaRef).toBe(externalRef)
+  })
+
+  it('resizes the textarea using external elementRef', () => {
+    const externalRef = { current: document.createElement('textarea') }
+    const { result } = renderHook(() => useTextareaAutoSize({ elementRef: externalRef }))
+
+    act(() => {
+      // Mocking the scrollHeight property
+      Object.defineProperty(externalRef.current, 'scrollHeight', {
+        writable: true,
+        value: 150, // Expected pixel height after content change
+      })
+
+      result.current.setInput('New text')
+      result.current.triggerResize()
+    })
+
+    expect(externalRef.current.style.height).toBe('150px')
+  })
+
+  it('resizes the textarea based on content with internal ref', () => {
+    render(<TextareaAutoSize />)
+
+    const textarea = screen.getByRole('textbox') as HTMLTextAreaElement
+
+    // Mocking the scrollHeight property
+    Object.defineProperty(textarea, 'scrollHeight', {
+      writable: true,
+      value: 150, // The pixel height you expect after changing the content
+    })
+
+    fireEvent.change(textarea, { target: { value: 'New text' } })
+
+    // It might be necessary to wait for next tick to allow for resize effect
+    setTimeout(() => {
+      expect(textarea.style.height).toBe('150px')
+    }, 0)
   })
 })
---
+++ src/components/Browser/TextareaAutoSize/TextareaAutoSize.tsx
@@ -0,0 +1,24 @@
+import React, { useState, ChangeEvent } from 'react'
+import useTextareaAutoSize from '../../../hooks/Browser/useTextareaAutoSize/useTextareaAutoSize'
+
+const TextareaAutoSize: React.FC = () => {
+  const [text, setText] = useState('')
+  const { textareaRef } = useTextareaAutoSize({ input: text })
+
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const newValue = event.target.value
+    setText(newValue)
+  }
+
+  return (
+    <textarea
+      className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+      ref={textareaRef}
+      value={text}
+      onChange={handleChange}
+      style={{ resize: 'none' }}
+    />
+  )
+}
+
+export default TextareaAutoSize
