980b7144a93169e41739c630bb3b227b8c70715b
Radiks Alijevs <dedalik@gmail.com>
2024-01-16 23:42:58 +0200

    Update useFavicon.tsx

--- src/hooks/Browser/useFavicon/useFavicon.tsx
+++ src/hooks/Browser/useFavicon/useFavicon.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
 export interface UseFaviconOptions {
   newIcon?: string
@@ -7,30 +7,23 @@ export interface UseFaviconOptions {
   doc?: Document
 }
 
-export type UseFaviconReturnType = [string, React.Dispatch<React.SetStateAction<string>>]
-
-const useFavicon = ({
-  newIcon = '',
-  baseUrl = '',
-  rel = 'icon',
-  doc = document,
-}: UseFaviconOptions = {}): UseFaviconReturnType => {
+const useFavicon = ({ newIcon = '', baseUrl = '', rel = 'icon', doc = document }: UseFaviconOptions = {}) => {
   const [favicon, setFavicon] = useState<string>(newIcon)
 
   const applyIcon = useCallback(
     (icon: string) => {
-      const elements = doc.head.querySelectorAll(`link[rel*="${rel}"]`)
-      if (!elements.length) {
-        const link = doc.createElement('link')
-        link.rel = rel
-        link.href = `${baseUrl}${icon}`
-        link.type = `image/${icon.split('.').pop()}`
-        doc.head.appendChild(link)
-      } else {
-        elements.forEach((el: Element) => {
-          const linkElement = el as HTMLLinkElement
-          linkElement.href = `${baseUrl}${icon}`
-        })
+      let linkElement = doc.head.querySelector(`link[rel*="${rel}"]`) as HTMLLinkElement
+
+      if (!linkElement) {
+        linkElement = doc.createElement('link')
+        linkElement.rel = rel
+        doc.head.appendChild(linkElement)
+      }
+
+      const iconUrl = `${baseUrl}${icon}`
+      if (linkElement.href !== iconUrl) {
+        linkElement.href = iconUrl
+        linkElement.type = `image/${icon.split('.').pop()}`
       }
     },
     [baseUrl, rel, doc],
