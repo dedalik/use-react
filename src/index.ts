3c72f7f6c755d239cdc1ff6065fa6ac7c9ab8d6f
Radiks Alijevs <dedalik@gmail.com>
2024-01-18 00:06:50 +0200

    Add use on mount (#2)
    * Add useOnMount
    
    * 1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
+import useOnMount from './hooks/State/useOnMount/useOnMount'
 import useFavicon from './hooks/Browser/useFavicon/useFavicon'
 import useTextareaAutoSize from './hooks/Browser/useTextareaAutoSize/useTextareaAutoSize'
 
-export { useTextareaAutoSize, useFavicon }
+export { useTextareaAutoSize, useFavicon, useOnMount }
