1320d1f5e7548047bfb65f78a3f770011e79abab
Radiks Alijevs <dedalik@gmail.com>
2024-01-22 00:12:18 +0200

    Add useHash

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import useOnMount from './hooks/State/useOnMount/useOnMount'
 import useFavicon from './hooks/Browser/useFavicon/useFavicon'
 import useTextareaAutoSize from './hooks/Browser/useTextareaAutoSize/useTextareaAutoSize'
+import useHash from './hooks/Elements/useHash'
 
-export { useTextareaAutoSize, useFavicon, useOnMount }
+export { useTextareaAutoSize, useFavicon, useOnMount, useHash }
